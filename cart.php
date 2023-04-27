9ba3a2455b737ff4547d89b950058def7add6a71
Fathyy <fathiabdi254@gmail.com>
2023-04-27 23:01:18 +0300

    Created a shopping cart basket that shows the number of items in the cart

--- cart.php
+++ cart.php
@@ -8,13 +8,13 @@ if (isset($_POST['add_to_cart'])) {
         // check if the product is already in the cart
         if (in_array($_GET['id'], $session_array_id)) {
             echo "<script>alert('Product is already in the cart!')</script>";
-            
+            echo "<script>window.location ='index.php'</script>"; 
         }
         else {
             // if the product is not in the cart, add in the session variable
             $count = count($_SESSION['cart']);
             $session_array = array(
-                'id' => $_GET['id']
+                'id' => $_GET['id'] 
             );
             $_SESSION['cart'][$count]=$session_array;
             print_r($_SESSION['cart']);
--- includes/navbar.php
+++ includes/navbar.php
@@ -1,4 +1,6 @@
-<?php require_once __DIR__ . '/header.php'; ?>
+<?php 
+// session_start();
+require_once __DIR__ . '/header.php'; ?>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -7,7 +9,7 @@
         <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
             <ul class="navbar-nav mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" href="index.php">Home</a>
+                    <a class="nav-link active" href="#">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Link</a>
@@ -16,8 +18,20 @@
             <!-- shopping cart icon with number of items inside -->
         </div>
         <div class="nav-item ms-auto">
-                    <a class="nav-link" href="cart.php"><i class="fa-solid fa-cart-shopping"></i></a>
-            </div>
+            <a class="nav-link" href="cart.php">
+                <i class="fa-solid fa-cart-shopping fa-lg" style="color: #de731b; position:relative;">
+                    <span class="badge" style="color:white; position:absolute; left:15px; bottom:0.5px; background-color:black;
+                    border-radius:50%;" >
+                <?php
+                if (isset($_SESSION['cart'])) {
+                    $count = count($_SESSION['cart']);
+                    echo $count;
+                }
+                ?>
+                </span>
+                </i>
+            </a>
+        </div>
     </div>
 </nav>
 <?php require_once __DIR__ . '/footer.php'; ?>
