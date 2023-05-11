8dea08cfed648d65e427a946139933febd68c874
Fathyy <fathiabdi254@gmail.com>
2023-05-11 22:40:31 +0300

    removed the table in cart php

--- cart.php
+++ cart.php
@@ -37,37 +37,68 @@ if (isset($_POST['add_to_cart'])) {
 
 <?php require_once __DIR__ . '/includes/header.php';?>
 <?php require_once __DIR__ . '/includes/navbar.php';?>
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <!-- display all items from the carts table for the
-            logged in user -->
-            <?php
-            $user_id = $_SESSION['auth_user']['id'];
-            $statement = $dbh->prepare("SELECT * FROM cart c, products p, users u WHERE c.user_id=u.user_id
-            AND c.product_id=p.product_id AND c.user_id ='$user_id'");
-            $statement->execute();
-            while ($row = $statement->fetch(PDO::FETCH_ASSOC)) :?>
+<div class="py-5">
+    <div class="container">
+        <div class="">
             <div class="row">
-                <!-- products image -->
-                <div class="col-md-2">
-                    <img src="<?php echo $row['image']?>" alt="">
-                </div>
-                <!-- product name -->
-                <div class="col-md-4">
-                    <h5><?php echo $row['name']?></h5>
-                </div>
-                <!-- product price -->
-                <div class="col-md-3">
-                    <h5><?php echo $row['price']?></h5>
-                </div>
-                <!-- cart quantity -->
-                <div class="col-md-3">
-                    <!-- <h5><?php echo $row['quantity']?></h5> -->
+                <div class="col-md-12">
+                    <!-- display all items from the carts table for the
+                    logged in user -->
+                        <div class="row align-items-center">
+                            <!-- products image -->
+                            <div class="col-md-2">
+                                <h4>Image</h4>
+                            </div>
+                            <!-- product name -->
+                            <div class="col-md-2">
+                                <h4>Product name</h4>
+                            </div>
+                            <!-- product price -->
+                            <div class="col-md-2">
+                                <h4>Price</h4>
+                            </div>
+                            <!-- cart quantity -->
+                            <div class="col-md-3">
+                                <h4>Quantity</h4>
+                            </div>
+                            <div class="col-md-3">
+                                <h4>Action</h4>
+                            </div>
+                        </div>
+                    
+                    <?php
+                    $user_id = $_SESSION['auth_user']['id'];
+                    $statement = $dbh->prepare("SELECT p.name, p.image, p.price, c.quantity FROM cart c, 
+                    products p WHERE c.product_id=p.product_id AND c.user_id ='$user_id'");
+                    $statement->execute();
+                    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) :?>
+                        <div class="card shadow mb-3">
+                            <div class="row align-items-center">
+                                <!-- products image -->
+                                <div class="col-md-2">
+                                    <img src="<?php echo $row['image']?>" width="80px" alt="">
+                                </div>
+                                <!-- product name -->
+                                <div class="col-md-2">
+                                    <h5><?php echo $row['name']?></h5>
+                                </div>
+                                <!-- product price -->
+                                <div class="col-md-2">
+                                    <h5>$<?php echo $row['price']?></h5>
+                                </div>
+                                <!-- cart quantity -->
+                                <div class="col-md-3">
+                                    <h5><?php echo $row['quantity']?></h5>
+                                </div>
+                                <!-- remove button -->
+                                <div class="col-md-3">
+                                    <button type="button" class="btn btn-danger">Remove</button>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endwhile ?>
                 </div>
             </div>
-     
-            <?php endwhile ?>
         </div>
     </div>
 </div>
